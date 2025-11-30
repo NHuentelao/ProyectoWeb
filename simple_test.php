c8c297ef0cfb996015b64c803fef4411b3a612b4
NHuentelao <nhuentelaoh@alumnos.ceduc.cl>
2025-11-29 23:45:27 -0300

    Remove debug scripts

--- simple_test.php
+++
@@ -1,6 +0,0 @@
-<?php
-// simple_test.php
-echo "<h1>El servidor PHP funciona correctamente</h1>";
-echo "<p>Si ves esto, Apache y PHP están bien.</p>";
-echo "<p>Versión de PHP: " . phpversion() . "</p>";
-?>
